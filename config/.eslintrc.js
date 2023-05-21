0faa41b658261a11966b9632408c976d235c10df
wuz1mu <kingaal93@gmail.com>
2023-05-21 15:27:20 +0700

    Configs

---
+++ config/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'semi': ['error', 'never'],
+  },
+  ignorePatterns: [ "./dist/*" ],
+}
---
+++ config/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: "/config/tsconfig.json"
+                    }
+                }],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+            useEslintrc: true,
+            rulePaths: ['./'],
+            overrideConfigFile: './config/.eslintrc.js'
+        })
+    ]
+};
